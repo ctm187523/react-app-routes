6d94fbb1435bd6b17be30fd9235fe8667fe7681d
CTM <clemente1875gmail.com>
2023-06-25 09:17:53 +0200

    cambios

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,19 +1,20 @@
-import { BrowserRouter } from "react-router-dom";
-import { Routes, Route, NavLink, Navigate } from "react-router-dom";
+import { Routes, Route, NavLink, Navigate, BrowserRouter } from "react-router-dom";
 import { ShoppingPage } from "../02-component-patterns/pages/ShoppingPage";
 
 import logo from '../logo.svg'
 export const Navigation = () => {
     return (
         <BrowserRouter>
+            {/* ver estilos de el classname main-layout en index.css */}
             <div className="main-layout">
+                {/* ver los estilos en index.css del nav */}
                 <nav>
                     <img src={logo} alt="React Logo" />
                     <ul>
                         <li>
                             {/* usamos el classname con una funcion de flecha que nos dice que si el link este activo muestre un estilo 
                             definido en el index.css usando una condicion terciaria*/}
-                            <NavLink to="/" className={ ({ isActive }) => isActive ? 'nav-active' : '' }>Shopping</NavLink>
+                            <NavLink to="/home" className={ ({ isActive }) => isActive ? 'nav-active' : '' }>Shopping</NavLink>
                         </li>
                         <li>
                             <NavLink to="/about" className={ ({ isActive }) => isActive ? 'nav-active' : '' }>About</NavLink>
@@ -27,7 +28,7 @@ export const Navigation = () => {
                 <Routes>
                     <Route path="about" element={<h1> About Page</h1>} />
                     <Route path="users" element={<h1> Users Page</h1>} />
-                    <Route path="/" element={ <ShoppingPage />} />
+                    <Route path="home" element={ <ShoppingPage />} />
 
                     {/* ponemos una ruta alternativa si no coincide la url mande al home
                     el replace es para que no pueda volver */}
